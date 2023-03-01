4284e48691424b1db1f41fa1c431446b1d291b51
Ian Dodds <ianmitchelldodds@gmail.com>
2023-03-01 17:29:23 -0500

    Connect to firebase

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,21 +1,47 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/Feed.css';
-import InputOption from './InputOption';
-import Post from './Post';
 import CreateIcon from '@mui/icons-material/Create';
 import ImageIcon from '@mui/icons-material/Image';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+import InputOption from './InputOption';
+import Post from './Post';
+import { firestore as db } from '../firebase';
+import firebase from '../firebase';
 
 
 function Feed() {
 
+  const [input, setInput] = useState('');
   const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection('posts')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot(snapshot => 
+        setPosts(
+          snapshot.docs.map(doc => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+  }, []);
 
   const sendPost = (event) => {
     event.preventDefault();
-  }
+
+    db.collection('posts').add({
+      name: 'Ian Dodds',
+      description: 'This is the description',
+      message: input,
+      photoURL: '',
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    setInput('');
+  };
 
   return (
     <div className='feed'>
@@ -23,7 +49,7 @@ function Feed() {
         <div className='feed__input'>
           <CreateIcon />
           <form>
-            <input type='text' />
+            <input value={input} onChange={(event) => setInput(event.target.value)} type='text' />
             <button onClick={sendPost} type='submit'>Send</button>
           </form>
         </div>
@@ -36,14 +62,21 @@ function Feed() {
       </div>
 
       {/* Posts */}
-      {posts.map((post) => (
-        <Post />
+      {posts.map(({ id, data: { name, description, message, photoURL }}) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoURL={photoURL}
+        />
       ))}
-      <Post
+
+      {/* <Post
         name='Ian Dodds'
         description='This is the description'
         message='This is the message'
-      />
+      /> */}
 
     </div>
   )
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBK8LuAShNg4zJSMBKspsLNCkPLhXqsu30",
+  authDomain: "linkedin-clone-a56fb.firebaseapp.com",
+  projectId: "linkedin-clone-a56fb",
+  storageBucket: "linkedin-clone-a56fb.appspot.com",
+  messagingSenderId: "19030071445",
+  appId: "1:19030071445:web:a593110d098d71620c639c",
+  measurementId: "G-ENYWCEWT7E"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
+export default firebase;
