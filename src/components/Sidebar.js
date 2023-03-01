631b650ed7a28f01298403c140d0f25b9897b612
Ian Dodds <ianmitchelldodds@gmail.com>
2023-03-01 14:14:02 -0500

    Add Sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Avatar from '@mui/material/Avatar';
+import '../styles/Sidebar.css';
+
+function Sidebar() {
+
+  const recentItem = (topic) => (
+    <div className='sidebar__recentItem'>
+      <span className='sidebar__hash'>#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
+  return (
+    <div className='sidebar'>
+      <div className='sidebar__top'>
+        <img
+          src="https://images.unsplash.com/photo-1557683316-973673baf926?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1129&q=80"
+          alt=""
+        />
+        <Avatar className='sidebar__avatar' />
+        <h2>Ian Dodds</h2>
+        <h4>linkedin.com/in/ian-dodds/</h4>
+      </div>
+
+      <div className='sidebar__stats'>
+        <div className='sidebar__stat'>
+          <p>Who viewed you</p>
+          <p className='sidebar__statNumber'>1,263</p>
+        </div>
+        <div className='sidebar__stat'>
+          <p>Views on post</p>
+          <p className='sidebar__statNumber'>1,048</p>
+        </div>
+      </div>
+
+      <div className='sidebar__bottom'>
+        <p>Recent</p>
+        {recentItem('reactjs')}
+        {recentItem('programming')}
+        {recentItem('softwareengineering')}
+        {recentItem('design')}
+        {recentItem('developer')}
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
